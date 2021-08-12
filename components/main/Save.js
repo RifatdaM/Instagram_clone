4c50e92c52c104e2008be4cb6d08d768cc0ed6c9
Rakibul Hasan <rifat20089@hotmail.com>
2021-08-12 09:18:11 +0600

    photo still not uploading

--- components/main/Save.js
+++ components/main/Save.js
@@ -6,14 +6,14 @@ require("firebase/firestore");
 require("firebase/firebase-storage");
 
 export default function Save(props) {
+  console.log(props.route.params.image)
   const [caption, setCaption] = useState("");
 
 //   Image Upload
   const uploadImage = async () => {
     const uri = props.route.params.image;
 
-    const childPath =
-      `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`;
+    const childPath =`post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`;
     console.log(childPath);
 
     const respons = await fatch(uri);
@@ -22,23 +22,37 @@ export default function Save(props) {
     const task = firebase.storage().ref().child(childPath).put(blob);
 
     const taskProgress = snapshot => {
-      console.log(`transferred: ${snapshot.bytesTransferred}`);
+      console.log(`transferred: ${snapshot.bytesTransferred}`)
     };
 
     const taksCompleted = () => {
       task.snapshot.ref.getDownloadURL().then((snapshot) => {
-        console.log(snapshot);
-      });
+        savePostData(snapshot);
+        console.log(snapshot)
+      })
     };
 
     const taskError = snapshot => {
       console.log(snapshot);
     };
     task.on("state_changed", taskProgress, taskError, taksCompleted);
-  };
+  }
 
+  const savePostData = (downloadURL) => {
 
-
+    firebase.firestore()
+        .collection('posts')
+        .doc(firebase.auth().currentUser.uid)
+        .collection("userPosts")
+        .add({
+            downloadURL,
+            caption,
+            likesCount: 0,
+            creation: firebase.firestore.FieldValue.serverTimestamp()
+        }).then((function () {
+            props.navigation.popToTop()
+        }))
+}
   return (
     <View style={{ flex: 1 }}>
       <Image source={{ uri: props.route.params.image }} />
